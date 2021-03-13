927c88a72d9b81354e21be42144ce38e2d963968
oldluo-ray <luodianlei7@163.com>
2021-03-13 11:40:08 +0800

    拆分

--- day03-node/02-mongoose代码拆分/crud.js
+++ day03-node/02-mongoose代码拆分/crud.js
@@ -1,10 +1,13 @@
 ;(async function () {
   // 这个文件用来执行具体的增删改查
   const db = require('./db')
-  const { xxxModel } = require('./model')
-  // console.log(xxxModel)
-  // 此时,可能数据库还没有连接成功
+  const { xxxModel } = require('./model') //{xxxModel: model}
+  // 解构的代码:相当于下面的两行代码
+  // const obj= require('./model')
+  // const xxxModel = obj.xxxModel
+
   try {
+    // 此时,可能数据库还没有连接成功
     await db
     console.log('数据库连接成功')
     xxxModel.findOne({ age: 18 }, (err, data) => {
