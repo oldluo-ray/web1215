8b0dc392e7e02707d8fe72be01350bb59beec61a
oldluo-ray <luodianlei7@163.com>
2021-03-29 10:51:15 +0800

    添加

--- day14-react/01-react-web1215/src/App.js
+++ day14-react/01-react-web1215/src/App.js
@@ -7,28 +7,41 @@ export default class App extends Component {
   state = {
     list: [
       {
-        id: 3,
-        todoName: '吃饭',
-        isDone: true,
-      },
-      {
-        id: 2,
-        todoName: '睡觉',
-        isDone: true,
-      },
-      {
         id: 1,
-        todoName: '敲代码',
+        todoName: '吃饭',
         isDone: false,
       },
     ],
+  }
+  // 定义函数,用来接收header组件传递过来的任务名
+  getTodoName = (todoName) => {
+    // console.log(todoName)
+    // 定义任务对象,将数据添加到list中
+    const { list } = this.state
+
+    // 先判断list中是否有数据,如果没有,id就直接为1. 如果有就获取第一个,根据第一个的id的值 + 1 就是新的数据的id
+    let id = list.length ? list[0].id + 1 : 1
+
+    const todoObj = {
+      // id的值要求是整数,并且不重复
+      id,
+      todoName,
+      isDone: false,
+    }
+
+    list.unshift(todoObj)
+
+    this.setState({
+      list,
+    })
   }
   render() {
     const { list } = this.state
     return (
       <div className="todo-container">
         <div className="todo-wrap">
-          <Header></Header>
+          {/* 将接收任务名的函数,通过props传递给header */}
+          <Header getTodoName={this.getTodoName}></Header>
           <List list={list}></List>
           <Footer></Footer>
         </div>
--- day14-react/01-react-web1215/src/components/Header/Header.js
+++ day14-react/01-react-web1215/src/components/Header/Header.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react'
 import './Header.css'
 export default class Header extends Component {
+  state = {
+    todoName: '',
+  }
+
+  handle = (e) => {
+    this.setState({
+      todoName: e.target.value.trim(),
+    })
+  }
+
+  // 按下回车之后,调用app传过来的函数,将任务名传递给app
+  keyUpdaHandle = (e) => {
+    const { getTodoName } = this.props
+    const { todoName } = this.state
+    if (e.keyCode === 13) {
+      if (!todoName) return
+      //将todoName传递给app组件
+      getTodoName(todoName)
+      this.setState({
+        todoName: '',
+      })
+    }
+  }
+
   render() {
+    const { todoName } = this.state
     return (
       <div className="todo-header">
-        <input type="text" />
+        <input
+          type="text"
+          value={todoName}
+          onChange={this.handle}
+          onKeyUp={this.keyUpdaHandle}
+        />
       </div>
     )
   }
